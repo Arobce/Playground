03ace2fb3b80a8b7dd8e0bd9298caa555e122d38
Roshan <arobce@gmail.com>
2021-01-20 19:42:32 +0545

    Adds Gravity Canvas

--- JS/canvas-gravity/js/canvas.js
+++ JS/canvas-gravity/js/canvas.js
@@ -10,8 +10,13 @@ let mouse = {
 
 let maxRadius = 60;
 let minRadius = 2;
-
+let gravity = 1;
+let friction = 0.8;
 let colorArray = ["#D96E24", "#E09D48", "#E6D89D", "#89C2B5", "#00083E"];
+
+window.addEventListener("click",function(e){
+  init();
+});
 
 window.addEventListener("mousemove", function (e) {
   mouse.x = e.x;
@@ -25,7 +30,7 @@ window.addEventListener("resize", function () {
   init();
 });
 
-function Circle(x, y, radius, dx, dy) {
+function Ball(x, y, radius, dx, dy) {
   this.x = x;
   this.y = y;
   this.radius = radius;
@@ -33,7 +38,7 @@ function Circle(x, y, radius, dx, dy) {
   this.dy = dy;
   this.minRadius = radius;
   let color = colorArray[Math.floor(Math.random() * colorArray.length) - 1];
-
+  
   this.draw = function () {
     c.beginPath();
     c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
@@ -42,49 +47,30 @@ function Circle(x, y, radius, dx, dy) {
   };
 
   this.bounce = function () {
-    if (this.x + this.radius > innerWidth || this.x - this.radius < 0) {
-      this.dx = -this.dx;
+    if(this.y + this.radius > canvas.height){
+      this.dy = -this.dy * friction;
+    }else{
+      this.dy += gravity;
     }
-
-    if (this.y + this.radius > innerHeight || this.y - this.radius < 0) {
-      this.dx = this.dx;
-    }
-
-    this.x += this.dx;
     this.y += this.dy;
-
-    // Interactivity
-    if (
-      mouse.x - this.x < 50 &&
-      mouse.x - this.x > -50 &&
-      mouse.y - this.y < 50 &&
-      mouse.y - this.y > -50
-    ) {
-      if (this.radius < maxRadius) {
-        this.radius += 1;
-      }
-    } else if (this.radius > this.minRadius) {
-      this.radius -= 1;
-    }
-
     this.draw();
   };
 }
 
 let c = canvas.getContext("2d");
-let circles = [];
+let balls = [];
 function init() {
-  circles = [];
+  balls = [];
   for (let i = 0; i < 800; i++) {
     let radius = Math.random() * 20 + 1;
     let randomX = Math.random() * (innerWidth - radius * 2) + radius;
     let randomY = Math.random() * (innerHeight - radius * 2) + radius;
     let randomDx = (Math.random() - 0.5) * 5;
-    let randomDy = (Math.random() - 0.5) * 5;
+    let randomDy = (Math.random()) * 5;
 
-    let circle = new Circle(randomX, randomY, radius, randomDx, randomDy);
+    let ball = new Ball(randomX, randomY, radius, randomDx, randomDy);
 
-    circles.push(circle);
+    balls.push(ball);
   }
 }
 
@@ -92,8 +78,8 @@ function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, innerWidth, innerHeight);
 
-  for (let i = 0; i < circles.length; i++) {
-    circles[i].bounce();
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].bounce();
   }
 }
 
